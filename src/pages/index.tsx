53ef0802deb4b9eb749e413986ad4efbda2e98eb
kseehrich <kseehrich@gmail.com>
2025-11-24 20:14:25 +0100

    typos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -280,14 +280,14 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>DartBuddy - 501 Spiel Tracker</title>
+        <title>Dartkumpel - 501 Spiel Tracker</title>
         <meta name="description" content="Professional Dart 501 tournament tracker with large number pad for iPad tournaments" />
         <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
         
         {/* PWA Meta Tags */}
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-        <meta name="apple-mobile-web-app-title" content="DartBuddy" />
+        <meta name="apple-mobile-web-app-title" content="Dartkumpel" />
         <meta name="mobile-web-app-capable" content="yes" />
         
         {/* Icons */}
